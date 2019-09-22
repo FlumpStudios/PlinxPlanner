b4eec7b9225c0474e76793120bd4943fa49da284
Paul Marrable <flumpstudios@gmail.com>
2019-09-22 10:10:25 +0100

    Added a forth template

--- PlinxPlannerAdmin/Templates/Template3/runserver.bat
+++ PlinxPlannerAdmin/Templates/Template3/runserver.bat
@@ -1,5 +1,5 @@
 @echo "starting server"
 cd %~dp0
-start cmd /k python manage.py runserver 8001
+start cmd /k python manage.py runserver 8002
 PING localhost -n 6 >NUL
-start "" http://localhost:8001/admin
+start "" http://localhost:8002/admin
